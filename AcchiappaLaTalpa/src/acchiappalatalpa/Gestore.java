7594840a891994177ca5733888f89f62d72862c6
angelicapazzagli <pazzagli.angelica@franchettisalviani.net>
2026-03-03 18:26:54 +0100

    Relazione Buca-Gestore e override del metodo run dei Thread #3 #4
    Override del metodo run dei thread per poter gestire la logica di come una talpa appare e scompare da una buca e in quale(randomico).

--- AcchiappaLaTalpa/src/acchiappalatalpa/Gestore.java
+++ AcchiappaLaTalpa/src/acchiappalatalpa/Gestore.java
@@ -4,10 +4,37 @@
  */
 package acchiappalatalpa;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author pazzagli.angelica
  */
-public class Gestore {
-    
+public class Gestore extends Thread{
+    private ArrayList<Buca> buche;
+    private Random random;
+
+    public Gestore(ArrayList<Buca> buche) {
+        this.buche = buche;
+        this.random = new Random();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                int indice = random.nextInt(buche.size());
+                Buca buca = buche.get(indice);
+                if (!buca.presenzaTalpa()) {
+                    buca.mostraTalpa();
+                    Thread.sleep(1000);
+                    buca.nascondiTalpa();
+                }
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
