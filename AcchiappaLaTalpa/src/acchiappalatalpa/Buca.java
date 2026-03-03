4dc532296b877d0bcec50d4510c30edbb5e79b31
angelicapazzagli <pazzagli.angelica@franchettisalviani.net>
2026-03-03 18:18:38 +0100

    Logica classe Buca e collegamento ad un bottone #17

--- AcchiappaLaTalpa/src/acchiappalatalpa/Buca.java
+++ AcchiappaLaTalpa/src/acchiappalatalpa/Buca.java
@@ -4,10 +4,32 @@
  */
 package acchiappalatalpa;
 
+import javax.swing.JButton;
+
 /**
  *
  * @author pazzagli.angelica
  */
 public class Buca {
-    
+    private JButton bottone;
+    private boolean occupata;
+
+    public Buca(JButton bottone) {
+        this.bottone = bottone;
+        this.occupata = false;
+    }
+
+    public void mostraTalpa() {
+        occupata = true;
+        bottone.setText("TALPA!");
+    }
+
+    public void nascondiTalpa() {
+        occupata = false;
+        bottone.setText("");
+    }
+
+    public boolean presenzaTalpa() {
+        return occupata;
+    }
 }
